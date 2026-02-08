269dd7f4abadfd2046207e1777058bfac08db3b9
ofircampeanu1-netizen <ofircampeanu1@gmail.com>
2026-02-07 20:20:05 -0500

    Initialize main.jsx with React and Tasker component

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Tasker from '../Tasker.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Tasker />
+  </React.StrictMode>
+)
